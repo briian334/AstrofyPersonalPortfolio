0e07cd983efb5a93ba2e47845268432888ae301f
Brian Cervantes <PAX0brian.cervantes>
2025-07-05 16:15:13 -0600

    migrating astro i18n config to i18n starter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from "./src/locales";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrofy-template.netlify.app",
   integrations: [mdx(), sitemap()],
   i18n: {
-    defaultLocale: "es",
-    locales: ["es", "en"],
+    defaultLocale: DEFAULT_LOCALE_SETTING,
+    locales: Object.keys(LOCALES_SETTING),
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
   },
   vite: {
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Portfolio Website by Brian Cervantes";
-export const SITE_DESCRIPTION =
-	"Personal Portfolio Website by Brian Cervantes using Astrofy Template ⚡️";
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
 export const THEME_KEY = "theme";
@@ -12,3 +9,19 @@ export const LIGHT_THEME = "nord";
 export const LANG_KEY = "lang";
 export const SPANISH_LANG = "es";
 export const ENGLISH_LANG = "en";
+
+import type { Multilingual } from "@/i18n";
+
+export const SITE_TITLE: string | Multilingual = "Portfolio Website by Brian Cervantes";
+
+export const SITE_DESCRIPTION: string | Multilingual = {
+	es: "Portfolio Website por Brian Cervantes usando Astrofy Template ⚡️",
+	en: "Portfolio Website by Brian Cervantes using Astrofy Template ⚡️",
+};
+
+export const X_ACCOUNT: string | Multilingual = "@psephopaiktes";
+
+export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
+	en: "This page is not available in your language.",
+	es: "Esta página no está disponible en su idioma.",
+};
